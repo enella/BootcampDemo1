7b1c8b3b77d7f7ee612736da854c127e1f4438fb
enynaaa <enjav3@gmail.com>
2022-03-31 16:41:12 +0300

    SQL

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const mysql = require('express');
-const { listenerCount } = require('process');
+
+// luodaan yhteys
+
+const db = mysql.createConnection({
+    host : 'localhost',
+    user: 'root',
+    password: '',
+    database: 'memorySQL'
+});
+
+// yhdistä
+
+db.connect((err) => {
+    if (err) {
+        console.log("Erroooorr");
+        throw err;
+    }
+    console.log("MySQL toimii!");
+});
 
 const app = express();
 
