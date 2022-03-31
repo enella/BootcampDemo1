e48d20335cd2806640a985116f84529b1a0d943f
enynaaa <enjav3@gmail.com>
2022-03-31 16:29:35 +0300

    SQL

--- src/app.js
+++ src/app.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const mysql = require('express');
+const { listenerCount } = require('process');
+
+const app = express();
+
+app.listen('3306', () => {
+    console.log('Server running');
+});
