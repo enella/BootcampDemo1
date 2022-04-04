ef043bcdffda4476a84481c5deb54d05c1d29948
enynaaa <enjav3@gmail.com>
2022-04-04 20:52:59 +0300

    SQLconnection

--- src/game.js
+++ src/game.js
@@ -1,3 +1,6 @@
+/* jshint esversion: 6 */
+import {sendData} from "app.js";
+
 // app.js
 /* ASETUKET ----------------------------------------------------------------------------------------------- */
 var CONST = { // * vähän niinkuin asetukset
