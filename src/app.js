265da28c1c0a68c2170ca79be69bf023eee68023
enynaaa <enjav3@gmail.com>
2022-04-04 11:54:08 +0300

    SQL

--- src/app.js
+++ src/app.js
@@ -8,11 +8,6 @@ var con = mysql.createConnection({
 });
 
 con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  //var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
-  /*con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("1 record inserted");
-  });*/
-});
+    console.log("Connected!");
+  });
