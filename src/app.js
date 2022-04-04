92c98e064b42881be7b6ec1c3cc248d83529e7ee
enynaaa <enjav3@gmail.com>
2022-04-04 11:46:49 +0300

    SQLtest

--- src/app.js
+++ src/app.js
@@ -11,8 +11,8 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
   //var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
-  con.query(sql, function (err, result) {
+  /*con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("1 record inserted");
-  });
+  });*/
 });
